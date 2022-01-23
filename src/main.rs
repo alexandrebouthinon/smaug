0f3ceb73f313b737b0f386d6d7cfb66d4f351603
Alexandre Bouthinon <bouthinon.alexandre@gmail.com>
2022-01-23 02:32:22 +0100

    Add default Logger

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 mod api;
 
+use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer};
+use env_logger::Env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-  HttpServer::new(|| App::new())
+  env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+  HttpServer::new(|| App::new().wrap(Logger::default()))
     .bind("127.0.0.1:8000")?
     .run()
     .await
